47bc5783893989e765d00662e22439cc586d22de
M. Allen Jeffrey <73103636+jeffzy15@users.noreply.github.com>
2023-03-29 23:41:16 +0800

    feature: dispense pills at a given time

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def on_received_number(receivedNumber):
+    # For demonstration, it is set to 20. Actual value is 5000.
+    if receivedNumber < 20:
+        pins.digital_write_pin(DigitalPin.P8, 0)
+    else:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+radio.on_received_number(on_received_number)
+
+def on_button_pressed_a():
+    basic.show_string(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_received_string(receivedString):
     global fall
     if receivedString == "Fall":
@@ -5,28 +17,42 @@ def on_received_string(receivedString):
         music.set_volume(255)
         music.start_melody(music.built_in_melody(Melodies.JUMP_DOWN),
             MelodyOptions.FOREVER)
-        basic.show_leds("""
-            # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-        basic.pause(1)
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-        """)
-    elif receivedString == "Response":
-        fall = False
-        basic.clear_screen()
-        music.stop_melody(MelodyStopOptions.ALL)
+        for index in range(20):
+            basic.show_leds("""
+                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+            basic.pause(1)
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+            """)
     else:
-        pass
+        fall = False
+        music.stop_all_sounds()
 radio.on_received_string(on_received_string)
 
 fall = False
 radio.set_group(58)
+basic.show_string("hr:")
+
+def on_every_interval():
+    timeanddate.advance_by(1, timeanddate.TimeUnit.MILLISECONDS)
+loops.every_interval(1, on_every_interval)
+
+basic.show_string("hr:")
+while not (input.button_is_pressed(Button.B)):
+    if input.button_is_pressed(Button.A):
+        timeanddate.advance_by(1, timeanddate.TimeUnit.HOURS)
+    basic.show_string(timeanddate.time(timeanddate.TimeFormat.HMM))
+basic.show_string("min:")
+while not (input.button_is_pressed(Button.B)):
+    if input.button_is_pressed(Button.A):
+        timeanddate.advance_by(1, timeanddate.TimeUnit.MINUTES)
+    basic.show_string(timeanddate.time(timeanddate.TimeFormat.HMM))
--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     // For demonstration, it is set to 20. Actual value is 5000.
     if (receivedNumber < 20) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
     } else {
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
     }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Fall") {
@@ -33,5 +36,35 @@ radio.onReceivedString(function (receivedString) {
         music.stopAllSounds()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    // TO DEBUG: SERVO NOT WORKING
+    if (!(medicine)) {
+        takenMed = true
+        pins.servoWritePin(AnalogPin.P1, 90)
+    }
+})
 let fall = false
+let medicine = false
+let takenMed = false
 radio.setGroup(58)
+timeanddate.setTime(7, 30, 0, timeanddate.MornNight.AM)
+takenMed = false
+medicine = false
+loops.everyInterval(1, function () {
+    timeanddate.advanceBy(1, timeanddate.TimeUnit.Milliseconds)
+})
+basic.forever(function () {
+    if (timeanddate.time(timeanddate.TimeFormat.HMMAMPM) == "7:30am") {
+        if (!(takenMed)) {
+            music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+            basic.showString("Take pills")
+            medicine = true
+        }
+    } else if (timeanddate.time(timeanddate.TimeFormat.HMMAMPM) != "7:30am") {
+        if (!(takenMed)) {
+            music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+            basic.showString("Take pills")
+            medicine = true
+        }
+    }
+})
