ab7bdd1bf67fec970393d60768b90ee9480ccd15
M. Allen Jeffrey <73103636+jeffzy15@users.noreply.github.com>
2023-04-19 20:51:54 +0800

    change dispenser behaviour

--- main.ts
+++ main.ts
@@ -11,8 +11,21 @@ radio.onReceivedNumber(function (receivedNumber) {
         pins.digitalWritePin(DigitalPin.P8, 1)
     }
 })
+// to show that multiple pills can be dispensed
 input.onButtonPressed(Button.A, function () {
-    basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
+    if (grove.measureInCentimetersV2(DigitalPin.P2) <= 5) {
+        takenMed = true
+        pins.servoWritePin(AnalogPin.P15, 0)
+        basic.pause(2000)
+        pins.servoWritePin(AnalogPin.P15, 90)
+        medCount = medCount - 1
+        basic.showNumber(medCount)
+        basic.pause(2000)
+        basic.clearScreen()
+        if (medCount < 4) {
+            basic.showString("Top up medicine")
+        }
+    }
 })
 radio.onReceivedMessage(RadioMessage.Response, function () {
     fall = false
@@ -40,6 +53,9 @@ radio.onReceivedMessage(RadioMessage.Fall, function () {
             `)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == appointment) {
         basic.showString("Appointment!")
@@ -48,19 +64,23 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     // TO DEBUG: SERVO NOT WORKING
     if (!(takenMed)) {
-        if (grove.measureInCentimetersV2(DigitalPin.P2) <= 10) {
+        if (grove.measureInCentimetersV2(DigitalPin.P2) <= 5) {
+            alarm = false
             takenMed = true
-            pins.servoWritePin(AnalogPin.P1, 0)
+            pins.servoWritePin(AnalogPin.P15, 0)
             basic.pause(2000)
-            pins.servoWritePin(AnalogPin.P1, 90)
+            pins.servoWritePin(AnalogPin.P15, 90)
             medCount = medCount - 1
             basic.showNumber(medCount)
-            if (medCount < 5) {
+            basic.pause(2000)
+            basic.clearScreen()
+            if (medCount < 4) {
                 basic.showString("Top up medicine")
             }
         }
     }
 })
+let alarm = false
 let fall = false
 let medCount = 0
 let appointment = ""
@@ -71,17 +91,18 @@ timeanddate.setTime(7, 30, 0, timeanddate.MornNight.AM)
 takenMed = false
 appointment = "" + timeanddate.time(timeanddate.TimeFormat.HMMAMPM) + timeanddate.date(timeanddate.DateFormat.MD)
 basic.showString("" + (appointment))
-medCount = 25
+medCount = 0
 loops.everyInterval(1, function () {
     timeanddate.advanceBy(1, timeanddate.TimeUnit.Milliseconds)
 })
 basic.forever(function () {
-    if (timeanddate.time(timeanddate.TimeFormat.HMMAMPM) == "7:31am") {
-        if (!(takenMed)) {
-            music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
-            basic.showString("Take pills")
-        }
-    } else if (timeanddate.time(timeanddate.TimeFormat.HMMAMPM) != "7:32am") {
+    if (pins.analogReadPin(AnalogPin.P1) < 500) {
+        medCount += 1
+        basic.showNumber(medCount)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (timeanddate.time(timeanddate.TimeFormat.HMMAMPM) != "7:30am") {
         if (!(takenMed)) {
             music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
             basic.showString("Take pills")
